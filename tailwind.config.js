d7081cef1d6ca3538509bf75f006e20125c9fbc8
aminulwebdev <aminulwebdev@gmail.com>
2023-12-25 11:52:47 +0600

    Navbar added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,27 @@
 tailwind.config = {
-    theme: {
-      extend: {
-        colors: {
-          clifford: '#da373d',
-        }
-      }
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#FFD2A4',
+        'secondary': '#161722',
+        'sectionbg': '#FFF5EB',
+        'btnclr': '#1C1D28',
+        'navbg': '#0D0D0D',
+      },
+      fontFamily: {
+        'robotoCon': "'Roboto Condensed', sans-serif",
+        'poppins': "'Poppins', sans-serif",
+      },
+      fontSize: {
+        'sectionHeading': '48px',
+        'perasize': '16px',
+        'title': '24px',
+      },
+      maxWidth: {
+        'container': '1280px',
+      },
     }
-  } 
+  }
+}
+
+// font-family: 'Roboto Condensed', sans-serif;
